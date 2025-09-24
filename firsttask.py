90417a6f946dcd9e5e77596e6702adf202414666
juztpositive <juzztpositive@gmail.com>
2025-09-24 18:13:52 +0500

    Create firsttask.py

---
+++ firsttask.py
@@ -0,0 +1,11 @@
+year = 2024
+
+if year % 4 != 0:
+    print('Обычный год')
+elif year % 100 == 0:
+    if year % 400 == 0:
+        print('Високосный год')
+    else:
+        print('Обычный год')
+else:
+    print('Високосный год')
