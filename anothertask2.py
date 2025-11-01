773d00c4f3c5cca5d22253b621ad693bdcd0feb2
positive <juzztpositive@gmail.com>
2025-11-01 16:15:22 +0500

    Update

--- anothertask2.py
+++ anothertask2.py
@@ -1,5 +1,16 @@
 from datetime import datetime
+newspapers = {
+    'The Moscow Times': {'Date': 'Wednesday, October 2, 2002', 'Format': "%A, %B %d, %Y"},
+    'The Guardian': {'Date': 'Friday, 11.10.13', 'Format': "%A, %d.%m.%y"},
+    'Daily News': {'Date': 'Thursday, 18 August 1977', 'Format': "%A, %d %B %Y"},
+}
 
-print(f'The Moscow Times — {datetime.strptime('Wednesday, October 2, 2002', "%A, %B %d, %Y")}')
-print(f'The Guardian — {datetime.strptime('Friday, 11.10.13', "%A, %d.%m.%y")}')
-print(f'Daily News — {datetime.strptime('Thursday, 18 August 1977', "%A, %d %B %Y")}')
+while True:
+    command = input('Введите команду:\n')
+    if command != 'q':
+        try:
+            print(command + f' — {datetime.strptime(newspapers[command]['Date'], newspapers[command]['Format'])}')
+        except KeyError:
+            print('Неверная команда, попробуйте ввести правильное название газеты')
+    else:
+        break
