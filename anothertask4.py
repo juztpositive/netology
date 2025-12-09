0de903a3e806ed55b40fb1a5683e395d6c47b070
positive <juzztpositive@gmail.com>
2025-12-09 11:47:49 +0500

    Update anothertask4.py

--- anothertask4.py
+++ anothertask4.py
@@ -1,43 +1,44 @@
-info = open("web_clients_correct.csv").readlines()
-info.remove(info[0])
-tableinformation = []
+with open("web_clients_correct.csv") as file:
+    info = file.readlines()
 
-def checkage(age):
-    agescheck = ["0", "5", "6", "7", "8", "9"]
+info.remove(info[0])
+table_info = []
+
+def get_age(age):
+    ages_check = ["0", "5", "6", "7", "8", "9"]
 
     if len(age) >= 2 and age[-2:] in ["11", "12", "13", "14"]:
         return f"{age} лет"
-    if age[-1] in agescheck:
+    if age[-1] in ages_check:
         return f"{age} лет"
     elif age[-1] == "1":
         return f"{age} год"
     else:
         return f"{age} года"
 
-def checkdevice(device):
-    devicecheck = {
+def get_device(device):
+    device_check = {
         "mobile": "мобильного",
         "tablet": "мобильного",
         "laptop": "компьютерного",
         "desktop": "компьютерного"
     }
-    return devicecheck[device]
+    return device_check[device]
 
-def checkgender(gender):
+def is_female(gender):
     return gender == "female"
 
 for line in info:
-    splitedline = line.split(",")
-    username = splitedline[0]
-    isfemale = checkgender(splitedline[3])
+    info_split = line.split(",")
+    isfemale = is_female(info_split[3])
     gender = "женского" if isfemale else "мужского"
-    gendersecond = "совершила" if isfemale else "совершил"
-    age = checkage(splitedline[4])
-    device = checkdevice(splitedline[1])
-    browser = splitedline[2]
-    region = splitedline[6]
+    age_info = f'{get_age(info_split[4]) + (" совершила" if isfemale else " совершил")} покупку на'
+    device_info = f'с {get_device(info_split[1])} браузера {info_split[2]}'
+    region = f'Регион, из которого совершалась покупка: {info_split[6]}'
+    username_info = f'Пользователь {info_split[0] + ' ' + gender} пола'
 
-    string = f'Пользователь {username} {gender} пола, {age} {gendersecond} покупку на {splitedline[5]} у.е. с {device} браузера {browser}. Регион, из которого совершалась покупка: {region}'
-    tableinformation.append(string)
+    string = f'{username_info}, {age_info} {info_split[5]} у.е. {device_info}. {region}'
+    table_info.append(string)
 
-newfile = open("new.txt", "w", encoding="utf-8").writelines(tableinformation)
+with open("new2.txt", "w", encoding="utf-8") as info_newfile:
+    info_newfile.writelines(table_info)
